623230d784c4ccb86e50a12a3b8c1b86e3777b68
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2024-10-01 00:06:05 +0530

    homepage version 1

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Kronicle | All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
