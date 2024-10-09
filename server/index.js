85752132b3983536809d37badd67963d7a4cf863
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2024-10-09 23:36:02 +0530

    db issued solved

--- server/index.js
+++ server/index.js
@@ -5,17 +5,14 @@ dotenv.config({
     path: './.env'
 })
 
-app.listen(process.env.PORT|| 8000,()=>{
-    console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    
-})
 
-// connectDB()
-//   .then(() => {
-//     app.listen(process.env.PORT || 3000, () => {
-//       console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-//   });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
