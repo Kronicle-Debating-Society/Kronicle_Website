02f030fb1b51679e59e09fa0bcf461e0ca487914
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2024-10-11 23:15:09 +0530

    addded jwt and encryption

--- server/models/events.model.js
+++ server/models/events.model.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import moongooseAgregatePaginate from "mongoose-aggregate-paginate-v2"
+
 
 const eventSchema = new mongoose.Schema(
   {
     title: {
       type: String,
       required: true,
+    },
+    thumbnail:{
+      type:String, // cloudinary url 
+      required: true, 
     },
     description: {
       type: String,
@@ -35,8 +41,14 @@ const eventSchema = new mongoose.Schema(
       enum: ["online", "offline", "hybrid"], // Restrict to these modes
       required: true,
     },
+    regCount:{
+      type:Number,
+      default:0,
+    },
   },
   { timestamps: true }
 );
+
+eventSchema.plugin(moongooseAgregatePaginate)
 
 export const Event = mongoose.model("Event", eventSchema);
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 // schema definition
 const userSchema = new mongoose.Schema(
@@ -6,6 +8,7 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      index: true, // for better searching
     },
     email: {
       type: String,
@@ -21,10 +24,13 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true, // make sure you hash this in the logic
     },
-    memberType: {
+    refreshToken: {
       type: String,
+    },
+    membertype: {
+      type: [String], // Array of strings to allow multiple choices
       enum: ["member", "admin", "core", "coordinators"], // predefined multiple choices for member type
-      default: "member", // default option
+      default: ["member"], // default option as an array,
     },
     isMember: {
       type: Boolean,
@@ -35,6 +41,12 @@ const userSchema = new mongoose.Schema(
       type: String, // cloudinary URL
       required: true,
     },
+    eventHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Event,
+      },
+    ],
     igLink: String,
     linkedLink: String,
     githubLink: String,
@@ -42,5 +54,39 @@ const userSchema = new mongoose.Schema(
   { timestamps: true } // taking the timestamps
 );
 
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+//dont use arrow function because there is no refrence to this present so while using callback over here we use func(){}
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password); //boolean return 
+};
+userSchema.methods.generateAcessToken=function(
+){
+return  jwt.sign({
+    _id:this._id,
+    email:this.email
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+  return  jwt.sign({
+    _id:this._id,  
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+}
 export const User = mongoose.model("User", userSchema);
 // exporting schema
