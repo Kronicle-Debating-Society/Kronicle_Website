980e0f7ea3546d2cc72bb0455cbcd8c4d24f9601
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2024-10-12 14:42:41 +0530

    error resolved for frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Navbar from './Components/Navbar.jsx' 
 import './index.css'
-import Navbar from './src/Components/Navbar.jsx'
+import Footer from './Components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-<Navbar/>
+    <Navbar/>
     <App />
+ <Footer/>
   </StrictMode>,
 )
