89060a6a60d194dfe67fc312431c088cf0402160
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2024-10-17 21:31:44 +0530

    new achievement added

--- server/models/achivements.model.js
+++ server/models/achivements.model.js
@@ -1,27 +1,26 @@
 import mongoose from "mongoose";
 
-const achievementSchema = new mongoose.Schema({
+const achievementSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    date: {
-        type: Date,
-        default: Date.now,
+    picture: {
+      type: String,
+      required: true,
     },
-    location: {
-        type: String,
-        required: true, 
+    member: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // using  associated User model
+      required: true,
     },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // using  associated User model
-        required: true,
-    },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export const Achievement = mongoose.model("Achievement", achievementSchema);
