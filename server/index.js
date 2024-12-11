590a7d200d7fcbbfbc981b79296abef3c0f10543
Kevin26ng <ngangomkevin@gmail.com>
2024-12-11 20:46:17 +0530

    Up to Date

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
-const app = express()
+const app =express()
 const port = 3000
+const cors=require("cors")
+
+app.use(
+  cors({
+    origin:"http://localhost:3000"
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
+
