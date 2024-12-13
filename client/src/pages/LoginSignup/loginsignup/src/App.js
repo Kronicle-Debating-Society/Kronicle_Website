8dfc5c3454ccca1d3b6346831a72ac25d7b1f031
Kevin26ng <ngangomkevin@gmail.com>
2024-12-14 04:13:54 +0530

    Fixing error

--- client/src/pages/LoginSignup/loginsignup/src/App.js
+++ client/src/pages/LoginSignup/loginsignup/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from 'Kronicle_Website/client/src/pages/LoginSignup/loginsignup/src/Components/LoginSignup/LoginSignup.jsx'
+
 
 function App() {
   return (
